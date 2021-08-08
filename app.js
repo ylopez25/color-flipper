cf1ff2130d4e6fc271a8ea10c4148c4be8faf16e
Yesenia L <ylopezcr123@gmail.com>
2021-08-08 18:50:45 -0400

    index.html

---
+++ app.js
@@ -0,0 +1,15 @@
+// const colors= ["RGB(227, 3, 252)", "grey", "blue", "pink"];
+// const btn = document.getElementById("btn");
+// const color = document.querySelector(".color");
+
+// btn.addEventListener("click", function() {
+//     console.log(document.body)
+//     //to check what is being targeted 
+//     const randonNumber = getRandomNumber();
+//     document.body.style.backgroundColor= colors[randonNumber];
+//     color.textContent = colors[randonNumber]
+// })
+
+// function getRandomNumber() {
+//     return Math.floor(Math.random() * colors.length);
+// }
